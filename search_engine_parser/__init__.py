4c3d733e91b4c4a3d22a18114f8512f60d3d79cf
Amit Sharma <48654350+buddhhu@users.noreply.github.com>
2021-03-18 11:28:33 +0530

    Update __init__.py

--- search_engine_parser/__init__.py
+++ search_engine_parser/__init__.py
@@ -27,17 +27,11 @@ from search_engine_parser.core import engines
 # DEPRECATION_WARNING: These imports will be removed in later versions
 from search_engine_parser.core.engines.aol import Search as AolSearch
 from search_engine_parser.core.engines.ask import Search as AskSearch
-from search_engine_parser.core.engines.baidu import Search as BaiduSearch
-from search_engine_parser.core.engines.bing import Search as BingSearch
-from search_engine_parser.core.engines.duckduckgo import \
-    Search as DuckDuckGoSearch
+from search_engine_parser.core.engines.duckduckgo import Search as DuckDuckGoSearch
 from search_engine_parser.core.engines.github import Search as GithubSearch
 from search_engine_parser.core.engines.google import Search as GoogleSearch
-from search_engine_parser.core.engines.googlescholar import \
-    Search as GoogleScholarSearch
-from search_engine_parser.core.engines.stackoverflow import \
-    Search as StackOverflowSearch
 from search_engine_parser.core.engines.yahoo import Search as YahooSearch
+from search_engine_parser.core.engines.myanimelist import Search as AnimeSearch
 
 name = "search-engine-parser"  # pylint: disable=invalid-name
 __version__ = "0.6.2"
